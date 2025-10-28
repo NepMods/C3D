698bf148a42c24dfaf0c08aa50a0ea2065cafb2f
nepmods <nepmods@proton.me>
2025-10-28 12:29:28 +0545

    Light Added

--- src/engine/Mesh/mesh.c
+++ src/engine/Mesh/mesh.c
@@ -96,47 +96,6 @@ Mesh *initialize_mesh() {
     return mesh;
 }
 
-//
-// void add_object(Mesh *mesh, Object *object){
-//      mesh->objects = realloc(mesh->objects, sizeof(Object) * (mesh->num_objects + 1));
-//     if (!mesh->objects) {
-//         mesh->num_objects = 0;
-//         return;
-//     }
-//     // Copy the new Object into the array
-//     mesh->objects[mesh->num_objects] = object;
-//     mesh->num_objects++;
-// }
-//
-// void add_objects(Mesh *scene, Object **objects, int num_objects) {
-//     for (int i = 0; i < num_objects; i++) {
-//         add_object(scene, objects[i]);
-//     }
-// }
-// void remove_object(Mesh *mesh, Object *object) {
-//     if (!object ||mesh->num_objects == 0) return;
-//
-//     int found_index = -1;
-//     for (int i = 0; i <mesh->num_objects; i++) {
-//         if (memcmp(&mesh->objects[i], object, sizeof(Object)) == 0) {
-//             found_index = i;
-//             break;
-//         }
-//     }
-//     if (found_index == -1) return;
-//     for (int i = found_index; i <mesh->num_objects - 1; i++) {
-//         mesh->objects[i] = mesh->objects[i + 1];
-//     }
-//
-//     mesh->num_objects--;
-//     if (object->total_vertices > 0) {
-//         mesh->objects = realloc(mesh->objects, sizeof(Object) *mesh->num_objects);
-//     } else {
-//         free(mesh->objects);
-//         mesh->objects = NULL;
-//     }
-// }
-
 void on_update_mesh(Mesh *mesh ,update_function function) {
     mesh->customUpdate = function;
 }
--- src/engine/Mesh/mesh.h
+++ src/engine/Mesh/mesh.h
@@ -29,10 +29,7 @@ typedef struct {
 Mesh *initialize_mesh();
 Mesh* from_raw(float *vertices, int num_vertices, int *indices, int num_indices);
 void upload_mesh(Mesh *mesh);
-//
-// void add_object(Mesh *scene, Object *object);
-// void add_objects(Mesh *scene, Object **objects, int num_objects);
-// void remove_object(Mesh *scene, Object *object);
+
 void on_update_mesh(Mesh *mesh, update_function function);
 Mesh *load_from_file(char *filename);
 
